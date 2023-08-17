838846961824a2d0105c8a508280d0bfc99db049
Kirby <moreland838@gmail.com>
2023-08-17 00:45:33 -0500

    did the thing

--- hello.js
+++ hello.js
@@ -4,13 +4,13 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
-console.log("Hello");
+console.log("Hello, World!"); console.log("I feel strange"); console.log('quoth the raven"nevermore"');
 
 /*  
 Next, PLAY! 
 Modify your code on line 7 to try to accomplish the tasks listed below:
 
-   1. Change the message that is printed.
+console.log("Howdy")
    2. Figure out what the parentheses do. Will the code work without them?
    3. Remove one or both quotation marks. Do we need to include both opening and closing quote marks? Is there a difference between using a single or a double quote (' vs. ")?
    4. Remove the semi-colon, ;.
